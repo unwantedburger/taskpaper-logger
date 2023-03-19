1211cf865c55f600d4ca3b65524cfdcfd1c971e7
Staale Nataas <staale@evokedset.no>
2023-03-19 16:45:41 +0100

    Finally great logging

--- index.js
+++ index.js
@@ -16,22 +16,96 @@ async function readTaskPaperFile(fileUrl) {
 
   let date = new Date().toISOString().slice(0, 16).replace("T", "-");
 
-  let tasks = 0;
-  let projects = 0;
+  let tasks = {
+    total: 0,
+    critical: 0,
+    high: 0,
+    mid: 0,
+    low: 0,
+    evokedset: 0,
+    unprioritised: 0,
+    done: 0,
+  };
   let done = 0;
+  let projects = 0;
+
+  const valueStrings = ["@critical", "@high", "@mid", "@low"];
 
   lines.forEach((line) => {
     const trimmedLine = line.trimStart();
     if (trimmedLine.endsWith(":")) {
       projects++;
-    } else if (trimmedLine.startsWith("-") && !trimmedLine.includes("@done")) {
-      tasks++;
-    } else if (trimmedLine.includes("@done")) {
-      done++;
+      return;
+    } else if (trimmedLine.startsWith("-")) {
+      // These are all the tasks.
+
+      tasks.total++;
+
+      // If it contains @done write to done and move on
+
+      if (trimmedLine.includes("@done")) {
+        tasks.done++;
+        return;
+      }
+
+      // Loop through then valueStrings
+      let categorized = false;
+      for (let i = 0; i < valueStrings.length; i++) {
+        // If the current valuestring is included in the task.
+        if (trimmedLine.includes(valueStrings[i])) {
+          console.log(trimmedLine);
+          // Log it and break the loop.
+
+          console.log(valueStrings[i].slice(1));
+          tasks[valueStrings[i].slice(1)]++;
+          categorized = true;
+          break;
+        }
+      }
+      // If it contains evokedset pris stick it in evokedest
+      if (
+        trimmedLine.includes("@e1") ||
+        trimmedLine.includes("@e2") ||
+        trimmedLine.includes("@e3")
+      ) {
+        // console.log("yas");
+        tasks.evokedset++;
+        categorized = true;
+        return;
+      } else {
+        if (!categorized) {
+          // If it's not part of the array, log it as unlabelled
+
+          tasks.unprioritised++;
+          return null;
+        }
+      }
     }
   });
+  console.log({ tasks, projects });
 
-  return { date, tasks, projects, done };
+  checkSum(tasks);
+
+  return { date, tasks, projects };
+}
+
+function checkSum(tasks) {
+  // Remove the total from the object.
+  const { total, ...taskValues } = tasks;
+
+  // Iterate over and sum them up
+  let sum = 0;
+  for (let key in taskValues) {
+    sum += taskValues[key];
+  }
+
+  console.log({ sum, total });
+  // Compare with total
+  if (sum === total) {
+    console.log("Sum is equal to total.");
+  } else {
+    console.log("Sum is not equal to total.");
+  }
 }
 
 async function logTaskProjectCounts(data) {
